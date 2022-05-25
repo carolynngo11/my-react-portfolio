54daa776c63ad28a4f7fceec6f59f80e138faf6d
Carolyn Ngo <carolynngo11@yahoo.com>
2022-05-24 21:18:44 -0500

    edit file names, added coding

---
+++ src/components/ReactPortfolio.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import Home from './pages/Home';
+import Navigation from './pages/Navigation';
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  const renderPage = () => {
+    if (currentPage === 'Home') {
+      return <Home />;
+    }
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <Navigation currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+    </div>
+  );
+}
---
+++ src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my page!</h1>
+      <p>
+      My name is Carolyn Ngo. I have previously enrolled to SCTCC, NHCC, and St.Kates and graduated with an Associate's of Art degree at NHCC.
+      </p>
+    </div>
+  );
+}
